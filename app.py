953348126e2a93419082567cdaa40db98f636142
Gabriele De Rosa <derosagabriele@outlook.it>
2020-03-15 21:10:15 +0100

    Open and read files

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
 #!/usr/bin/env python3
 import sys
+import pysam
 
 def main(argv):
 
     # parameters
     filename = argv[1]
-    positions = [int(item) for item in argv[2:]]    
+    positions = [int(item) for item in argv[2:]]
+
+    # Open valid input files 
+    if '.sam' in filename.lower():
+        samfile = pysam.AlignmentFile(filename, "r")
+    elif '.bam' in filename.lower():
+        pysam.index(filename)
+        samfile = pysam.AlignmentFile(filename, "rb")
+    else:
+        print('Invalid input file.')
+        return
+
+    # Read data
+    iter = samfile.fetch()
+    for x in iter:
+        print(str(x))   
+
+    samfile.close()  
 
 if __name__ == '__main__':
     main(sys.argv)
