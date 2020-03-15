8b3504e5cc57bffc16f365baf4bf3a059529d6a6
Gabriele De Rosa <derosagabriele@outlook.it>
2020-03-15 21:05:13 +0100

    Init

---
+++ app.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import sys
+
+def main(argv):
+
+    # parameters
+    filename = argv[1]
+    positions = [int(item) for item in argv[2:]]    
+
+if __name__ == '__main__':
+    main(sys.argv)
