8d8e5411236a0be698b885e16f92a14ef0425efc
Gabriele De Rosa <derosagabriele@outlook.it>
2020-03-23 13:19:20 +0100

    Speed improvements

--- app.backup.py
+++ app.backup.py
@@ -10,7 +10,7 @@ def main(argv):
         description='Print reads from a SAM/BAM file filtered for start positions.'
     )
     
-    # Main features arguments 
+    # Main features
     parser.add_argument(
         '-f', '--file',
         required=True,
@@ -24,7 +24,7 @@ def main(argv):
         help='list of positions'
     )
 
-    # Extra features arguments
+    # Extra features
     parser.add_argument(
         '--only-pos',
         action='store_true',
@@ -51,41 +51,32 @@ def main(argv):
         print('An exception of type ' + type(ex).__name__ + ' occurred: ' + ex.args[1])
         return
 
+    # Read data
+    lines = samfile.fetch()
 
-    # Main features
-    if args.pos is None and not args.only_pos:
-        
-        # get all queries 
-        output = [line.query_alignment_sequence for line in samfile.fetch()]
-    
-    elif args.pos is not None and not args.only_pos: 
+    align = [str(line).strip().split('\t') for line in lines]
 
-        # get only queries of selected positions 
-        output = [line.query_alignment_sequence for line in samfile.fetch() if line.reference_start in args.pos]
+    # Filter
+    if args.pos is not None:
+        align = [line for line in align if int(line[3]) in args.pos]
 
-    # Extra features
-    elif args.pos is None and args.only_pos:
-
-        # get all positions 
-        output = [line.reference_start for line in samfile.fetch()]
-
-    elif args.pos is not None and args.only_pos: 
-
-        # get only selected positions
-        output = [line.reference_start for line in samfile.fetch() if line.reference_start in args.pos]
-
+    # Get only positions available
+    if(args.only_pos):
+        positions_list = [int(line[3]) for line in align]
+        print(positions_list)
+        return
 
     # Output requested data 
     print('OUTPUT DATA: \n')
 
-    if len(output) == 0:
+    if len(align) == 0:
         print('No reads found.')
     else:
-        print(output)
+        for x in align:
+            print(x[9] + ' (' + x[3] + ') \n') # query (pos)
 
     # Close file 
-    samfile.close()
-
+    samfile.close()  
 
 if __name__ == '__main__':
     main(sys.argv)
